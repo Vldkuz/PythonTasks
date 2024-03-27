d7e44bb795b322c7b157de0fca0d48fb4fb03bbd
Семен Кузнецов <semen33p@rambler.ru>
2024-03-27 09:24:53 +0500

    Промежуточный фронт

--- SeaBattle/src/front/front.py
+++ SeaBattle/src/front/front.py
@@ -1,8 +1,10 @@
-import sys
+from PyQt6.QtGui import QColor, QMouseEvent
+from PyQt6.QtWidgets import QWidget, QPushButton, QLabel, QVBoxLayout, \
+    QGridLayout, QHBoxLayout
+from PyQt6.QtCore import Qt
 from screeninfo import get_monitors
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, \
-    QVBoxLayout, QGridLayout, QHBoxLayout
-from PyQt6.QtGui import QColor, QScreen
+from SeaBattle.src.backend.player import Player
+from SeaBattle.src.backend.game import Game
 
 NUMBER_OF_COLUMN = 10
 NUMBER_OF_ROWS = 10
@@ -16,7 +18,7 @@ INDENT_Y = int(monitor.height * INDENT_Y_COFF)
 
 WIDTH_COFF = 0.625
 HEIGHT_COFF = 0.55555555555555555555555555555556
-WIDTH_X = int(monitor.width * WEIDTH_COFF)
+WIDTH_X = int(monitor.width * WIDTH_COFF)
 HEIGHT_Y = int(monitor.height * HEIGHT_COFF)
 
 WIDTH_BUTTON_X = 50
@@ -31,32 +33,30 @@ ASCII_START = 65
 
 FLAG = 0
 
+RED_COLOR = 'red'
+BLUE_COLOR = 'blue'
+WHITE_COLOR = 'white'
+GRAY_COLOR = 'gray'
+
 
 class ColorChanger(QWidget):
 
+    # создание оси столбцов
     @staticmethod
     def create_cols(grid_layout):
-
         for j in range(NUMBER_OF_COLUMN):
             label = QLabel(str(j + GLOBAL_STEP))
             grid_layout.addWidget(label, j + GLOBAL_STEP, FLAG)
 
+    # создание оси строк
     @staticmethod
     def create_rows(grid_layout):
         for i in range(NUMBER_OF_ROWS):
             label = QLabel(chr(ASCII_START + i))
             grid_layout.addWidget(label, FLAG, i + GLOBAL_STEP)
 
-    @staticmethod
-    def create_button(grid_layout, i, j, change_color, buttons, number):
-        button = QPushButton()
-        button.setMaximumSize(WEIDTH_BUTTON_X, WEIDTH_BUTTON_Y)
-        button.setStyleSheet('background-color: white;')
-        button.clicked.connect(lambda _, row=i, col=j, grid_num=number:
-                               change_color(row, col, grid_num))
-        grid_layout.addWidget(button, i + GLOBAL_STEP, j + GLOBAL_STEP)
-        buttons.append(button)
-
+    # =========================================================================
+    # создание всех кнопок. // из __init__
     def create_grid(self, grid_layout1, grid_layout2):
         for i in range(NUMBER_OF_ROWS):
             for j in range(NUMBER_OF_COLUMN):
@@ -65,6 +65,28 @@ class ColorChanger(QWidget):
                 self.create_button(grid_layout2, i, j, self.change_color,
                                    self.buttons2, FIELD_2)
 
+    # создание одной кнопки
+    @staticmethod
+    def create_button(grid_layout, i, j, change_color, buttons, number):
+        button = QPushButton()
+        button.setMaximumSize(WIDTH_BUTTON_X, WIDTH_BUTTON_Y)
+        button.setStyleSheet('background-color: blue;')
+
+        button.clicked.connect(lambda _, row=i, col=j, grid_num=number:
+                               change_color(row, col, grid_num))
+        grid_layout.addWidget(button, i + GLOBAL_STEP, j + GLOBAL_STEP)
+        buttons.append(button)
+
+    def change_color(self, row, col, grid_num):
+        if grid_num == FIELD_1:
+            button = self.buttons1[row * NUMBER_OF_ROWS + col]
+        else:
+            button = self.buttons2[row * NUMBER_OF_COLUMN + col]
+        button.setStyleSheet(
+            f'background-color: {self.current_color.name()};')
+
+    # =========================================================================
+    # задание цвета всем кнопкам. // из __init__
     def create_color_buttons(self, colors_layout):
         for idx, c in enumerate(self.colors):
             btn = QPushButton(f'Color {idx + GLOBAL_STEP}', self)
@@ -72,44 +94,43 @@ class ColorChanger(QWidget):
                 lambda _, color=c: self.change_current_color(color))
             colors_layout.addWidget(btn)
 
+    # изменить текущий цвет
+    def change_current_color(self, color):
+        self.current_color = color
+
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle(NAME_WINDOW)
-        self.setGeometry(INDENT_X, INDENT_Y, WEIDTH_X, HEIGHT_Y)
+        self.setGeometry(INDENT_X, INDENT_Y, WIDTH_X, HEIGHT_Y)
 
         main_layout = QHBoxLayout()
         self.setLayout(main_layout)
 
+        # добавление поля обороны
         grid_layout1 = QGridLayout()
         main_layout.addLayout(grid_layout1)
 
+        # добавление поля атаки
         grid_layout2 = QGridLayout()
         main_layout.addLayout(grid_layout2)
 
+        # добавление кнопок
         colors_layout = QVBoxLayout()
         main_layout.addLayout(colors_layout)
 
+        # создание осей координат
         self.create_rows(grid_layout1)
         self.create_cols(grid_layout1)
         self.create_rows(grid_layout2)
         self.create_cols(grid_layout2)
 
-        # Добавляем кнопки выбора цвета
         self.buttons1 = []
         self.buttons2 = []
-        self.colors = [QColor('red'), QColor('green'), QColor('blue')]
+        self.colors = [QColor(RED_COLOR), QColor(BLUE_COLOR),
+                       QColor(WHITE_COLOR), QColor(GRAY_COLOR)]
         self.current_color = self.colors[FLAG]
 
+        # вывод на экран кнопок и полей
         self.create_grid(grid_layout1, grid_layout2)
         self.create_color_buttons(colors_layout)
-
-    def change_color(self, row, col, grid_num):
-        if grid_num == FIELD_1:
-            button = self.buttons1[row * NUMBER_OF_ROWS + col]
-        else:
-            button = self.buttons2[row * NUMBER_OF_COLUMN + col]
-        button.setStyleSheet(f'background-color: {self.current_color.name()};')
-
-    def change_current_color(self, color):
-        self.current_color = color
